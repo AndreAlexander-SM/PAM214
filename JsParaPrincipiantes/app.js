a95775d6776a449dfd4663100221cf3a7b1ca008
AndreAlexander-SM <124051521@upq.edu.mx>
2025-09-10 18:51:51 -0600

    Explicacion de tipo de datos

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,1 +1,24 @@
-document.write("<h1>Hello Word</h1>");
+// Tipo de datos 
+"Hellow Word" // string
+'Hellow Word' // string
+
+1000000 // number
+-2.3
+
+// boolean
+true
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1,2,3]
+[true, false, true, false]
+
+// object
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+}
