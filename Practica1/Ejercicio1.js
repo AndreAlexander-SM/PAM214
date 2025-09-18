18b34f7c5653c37d27dd8d469a448ae924116df7
AxelHm257 <123045383@upq.edu.mx>
2025-09-17 23:11:12 -0600

    Primer ejercicio
    cambie de var a let y a const

---
+++ Practica1/Ejercicio1.js
@@ -0,0 +1,10 @@
+const nombre = "Armando";
+let edad = 25;
+
+//nombre = "Ana Maria";
+
+let saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+//muestra en consola en contenido de saludo
+
+console.log(saludo);
