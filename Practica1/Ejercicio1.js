5ab4ed579eb033f9fb9e98facb897798f817f5c7
AxelHm257 <123045383@upq.edu.mx>
2025-09-17 23:39:16 -0600

    Segundo ejercicio
    Lo que hice fue crear una función flecha que pudiera multiplicar el mismo número por si mismo

--- Practica1/Ejercicio1.js
+++ Practica1/Ejercicio1.js
@@ -8,3 +8,22 @@ let saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 //muestra en consola en contenido de saludo
 
 console.log(saludo);
+
+// Ejercicio 2
+
+/*function cuadrado (numero){
+    return numero * numero;
+}
+*/
+
+//console.log();
+//muestra en la consola con 3 numeros diferentes
+
+const cuadrado = (numero) => numero * numero;
+
+let resultado1 = cuadrado(6);
+console.log(resultado1);
+let resultado2 = cuadrado(9);
+console.log(resultado2);
+let resultado3 = cuadrado(3);
+console.log(resultado3);
