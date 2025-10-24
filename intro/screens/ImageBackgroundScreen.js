f49da289c339f9134361264846c8217a499a657b
AndreAlexander-SM <124051521@upq.edu.mx>
2025-10-24 15:22:47 -0600

    ImageBackgroung & SlapshScreen: Completada
    Practica completada

--- intro/screens/ImageBackgroundScreen.js
+++ intro/screens/ImageBackgroundScreen.js
@@ -1,13 +1,74 @@
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, SafeAreaView, ImageBackground } from 'react-native'
+import {useEffect, useState} from 'react'
 
 export default function ImageBackgroundScreen() {
-  
+  const [showSplash, setShowSplash] = useState(true)
+  useEffect (() => {
+    const timer = setTimeout (() => {
+      setShowSplash(false);
+    },1000);
+    return() => clearTimeout(timer);
+
+  },[]);
+    if(showSplash){
     return (
-      <View>
-        <Text>Proximamente by Karla & equipo</Text>
+      <View style={styles.splashContainer}>
+        <Text style={styles.splashText}>Bienvenido a la app!</Text>
       </View>
-    )
+    );
+    }
+    return(
+      <SafeAreaView style={{flex:1}}>
+        <ImageBackground
+        source ={{
+          uri: 'https://wallpapercave.com/wp/wp3850825.jpg'
+        }}
+        style={styles.background}
+        >
+          <View style={styles.overlay}>
+            <Text style={styles.title}>Bienvenido!</Text>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    );
   }
 
-const styles = StyleSheet.create({})
+// Estilos
+const styles = StyleSheet.create({
+  
+
+  splashContainer: {
+    flex: 1, 
+    backgroundColor: '#000000ff', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+
+  
+  splashText: {
+    fontSize: 24, 
+    color: '#fff', 
+  },
+
+  
+  background: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+
+  
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+    padding: 20, 
+    borderRadius: 10, 
+  },
+
+  
+  title: {
+    fontSize: 28, 
+    color: '#fff', 
+    marginBottom: 10, 
+    textAlign: 'center', 
+  },
+});
