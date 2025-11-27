d443c8a045d4102ecb7847bf834381b5940f5af7
AndreAlexander-SM <124051521@upq.edu.mx>
2025-11-27 14:57:58 -0600

    Insert y Select funcionando

---
+++ BaseDatos/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+// Importamos la pantalla principal desde la carpeta screens
+import InsertUsuarioScreen from './screens/InsertUsuarioScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <InsertUsuarioScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
